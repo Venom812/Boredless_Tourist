9aa7ea9d1d87c632166398091ac08ffcc0caf2ac
Venom812 <skorodeev@gmail.com>
2021-10-19 01:30:24 +0300

    Added test objects

---
+++ script.py
@@ -0,0 +1,2 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
